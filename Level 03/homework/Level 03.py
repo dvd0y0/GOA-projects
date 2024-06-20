354b486597c1f7160668c433f2e8ce31600823a1
dyd0y0 <dvdmetskhovrishvili@gmail.com>
2024-06-20 14:07:54 +0400

    second commit

---
+++ Level 03/homework/Level 03.py
@@ -0,0 +1,25 @@
+name = "daviti"
+lastname = "metskhovrishvili"
+age = 13
+hobi = "sleep"
+country = "Georgia"
+
+print(name)
+print(lastname)
+print(age)
+print(hobi)
+print(country)
+
+
+#task 2
+
+print(10 + 10) 
+print(20 - 10) 
+print(5 * 5) 
+print(13 // 2)
+print(6 / 3 )
+print(20 + 11) 
+print(60 - 30) 
+print(7 * 6) 
+print(4 // 2)
+print(9 / 3 )
---
+++ Level 04/homework/Level 04.py
@@ -0,0 +1,7 @@
+name = "daviti"
+lastname = "metskhovrishvili"
+age = 13
+hobi = "sleep"
+country = "Georgia"
+
+print(name + " " + lastname + " " + str(age) + " " + hobi + " " + country )
