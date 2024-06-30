469456916b76565580bda822f2d0f1a02dca7195
dyd0y0 <dvdmetskhovrishvili@gmail.com>
2024-06-30 20:44:11 +0400

    second commit

---
+++ Level 07/homework/Level 07.py
@@ -0,0 +1,30 @@
+print(7 + 6)
+
+print(6 // 2)
+
+print(9 - 5)
+
+print(5 / 8)
+
+print(2 * 4)
+
+
+r = 5
+
+print(r + 8)
+
+t = 6
+
+print(t // 3)
+
+e = 9
+
+print(e - 4)
+
+f = 8
+
+print(f / 5)
+
+g = 4
+
+print(g * 6)
