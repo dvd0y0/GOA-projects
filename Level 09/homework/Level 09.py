9b7ce56dbf98b2ea6bd0fc6d3a3a1dc27e03ce9f
dyd0y0 <dvdmetskhovrishvili@gmail.com>
2024-07-10 14:19:00 +0400

    second commit

---
+++ Level 09/homework/Level 09.py
@@ -0,0 +1,42 @@
+print(23 > 12)
+
+print(56 > 43)
+
+print(33 > 12)
+
+print(23 > 72)
+
+print(11 > 42)
+
+print(63 > 62)
+
+print(83 > 52)
+
+print(43 > 12)
+
+print(93 > 72)
+
+print(92 > 23)
+
+print(51 > 43)
+
+print(83 > 56)
+
+print(43 > 71)
+
+print(82 > 48)
+
+print(26 > 64)
+
+print(76 > 73)
+
+print(55 > 33)
+
+print(44 > 22)
+
+print(89 > 33)
+
+print(54 > 39)
+
+print(71 > 81)
+
---
+++ Level 09/homework/Level 09.py
@@ -0,0 +1,7 @@
+my_age = int(input("Entrr your age: "))
+friend_age = int(input("Enter your friend age: "))
+
+if my_age > friend_age:
+    print("You are older then your friend")
+else:
+    print("your friend is older then you")
